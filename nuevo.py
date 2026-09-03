bc7ede3a5c50f448fb272aba40690bd473b49295
miguel <seguroluismiguel@gmail.com>
2026-09-03 15:32:13 -0500

    feat: agregar función dividir

---
+++ nuevo.py
@@ -0,0 +1,15 @@
+# Calculadora - Funciones adicionales
+
+def sumar(a, b):
+    return a + b
+
+def restar(a, b):
+    return a - b
+
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    if b == 0:
+        return "Error: no se puede dividir entre cero"
+    return a / b
